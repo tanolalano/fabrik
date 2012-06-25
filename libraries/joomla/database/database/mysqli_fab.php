7a180342f3c1f132d70f2473a88f5150f2cdb79c
pollen8 <rob@pollen-8.co.uk>
2012-06-25 18:36:24 +0200

    added logging to mysqli db driver execute()

--- libraries/joomla/database/database/mysqli_fab.php
+++ libraries/joomla/database/database/mysqli_fab.php
@@ -41,4 +41,47 @@ class JDatabaseMySQLi_Fab extends JDatabaseMySQLi
 		return parent::replacePrefix($sql, $prefix);
 	}
 
+	
+	public function execute()
+	{
+		//should we log the query (ingore select statements)
+		$run = true;
+		if (is_object($this->sql))
+		{
+			if (!is_null($this->sql->select))
+			{
+				$run = false;
+			}
+		}
+		else
+		{
+			if (strtoupper(JString::substr($this->sql, 0, 6)) === 'SELECT'
+			|| strtoupper(JString::substr($this->sql, 0, 4)) === 'SHOW'
+			)
+			{
+				$run = false;
+			}
+		}
+		if ($run)
+		{
+			$user = JFactory::getUser();
+			// Include the JLog class.
+			jimport('joomla.log.log');
+			$userName = $user->get('username');
+			// Add the logger.
+			$date = JFactory::getDate()->format('Y-m-d');
+			$opts = array(
+				'logger' => 'liquibase', 
+				'text_file' => 'dbchangeset/' . $date . '_' . $userName . '_' . uniqid() . '.sql',
+				'user' => $userName,
+				'id' => uniqid()
+			);
+			JLog::addLogger($opts, JLog::INFO, array('com_fabrik'));
+			
+			// start logging...
+			JLog::add($this->replacePrefix($this->sql), JLog::INFO, 'com_fabrik');
+		}
+		return parent::execute();
+	}
+	
 }
